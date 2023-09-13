4745dbaebf56fbbf9a027188bca1452c7353b329
tomaseliasfontes <tomaseliasfontes@gmail.com>
2023-09-13 20:37:00 -0300

    change names of js and css

--- week_01/scripts/script.js
+++
@@ -1,6 +0,0 @@
-const d = new Date();
-const year = d.getFullYear();
-document.getElementById("currentyear").innerHTML = `${year}`;
-
-let date = document.lastModified;
-document.getElementById("result").innerHTML = `Last modified : ${date}`;
