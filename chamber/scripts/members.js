13f55c4b6f197bc46bd67dc36ad94dff39a671f7
tomaseliasfontes <tomaseliasfontes@gmail.com>
2023-11-11 21:53:08 -0300

    add link to js members and fix members js

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -1,40 +1,70 @@
 const url = 'https://tomaseliasfontes.github.io/wdd230/chamber/data/members.json';
-const cards = document.querySelector('#cards');
+const cards = document.querySelector('.grid');
+const grid = document.querySelector("#grid");
+const list = document.querySelector("#list");
 
 async function getProphetData(url) {
     const response = await fetch(url);
     //check to see if the fetch was successful
     const data = await response.json();
-    displayProphets(data.prophets); // note that we reference the prophets array of the JSON data object, not just the object
+    displayMembers(data.members); // note that we reference the prophets array of the JSON data object, not just the object
 }
 
-const displayProphets = (prophets) => {
-    prophets.forEach((prophet) => {
+const displayMembers = (members) => {
+    members.forEach((member) => {
         let card = document.createElement("section");
-        let fullName = document.createElement("h2");
-        let portrait  = document.createElement("img");
-        let dateOfBirth = document.createElement("p");
-        let placeOfBirth = document.createElement("p");
+        let name = document.createElement("h2");
+        let address  = document.createElement("p");
+        let phone = document.createElement("a");
+        let pageUrl = document.createElement("a");
+        let membership = document.createElement("p");
+        let logo = document.createElement("img");
 
-        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+        card.setAttribute('class', 'member-card');
 
-        portrait.setAttribute("src", prophet.imageurl);
-        portrait.setAttribute("alt", `Picture of ${prophet.name} ${prophet.lastName}`);
-        portrait.setAttribute("loading", "lazy");
-        portrait.setAttribute("width", "340");
-        portrait.setAttribute("height", "440");
+        name.setAttribute('class', 'm-name');
+        name.textContent = member.name;
 
-        dateOfBirth.textContent = `Date of Birth: ${prophet.birthdate}`;
-        placeOfBirth.textContent = `Plave of Birth: ${prophet.birthplace}`;
+        address.setAttribute('class', 'm-address');
+        address.textContent = member.address;
 
-        card.appendChild(fullName);
-        card.appendChild(portrait);
+        phone.setAttribute('class', 'm-phone');
+        phone.textContent = member.phone;
+        phone.setAttribute('href', `tel:${member.phone}`);
 
-        card.appendChild(dateOfBirth);
-        card.appendChild(placeOfBirth);
+        pageUrl.setAttribute('class', 'm-url');
+        pageUrl.textContent = member.url;
+        pageUrl.setAttribute('href', `${member.url}`);
+        pageUrl.setAttribute('target', '_blank');
+
+        logo.setAttribute('class', 'main-logo');
+        logo.setAttribute('src', member.logo);
+        logo.setAttribute('alt', `Logo of ${member.name}`);
+        logo.setAttribute('loading', 'lazy');
+
+        membership.setAttribute('class', 'm-lvl');
+        membership.textContent = member.membershipLvl;
+
+
+
+        card.appendChild(name);
+        card.appendChild(logo);
+        card.appendChild(address);
+        card.appendChild(phone);
+        card.appendChild(pageUrl);
          
         cards.appendChild(card);
     });
 }
   
 getProphetData(url);
+
+grid.addEventListener("click", () => {
+    cards.classList.add("grid");
+    cards.classList.remove("list");
+})
+
+list.addEventListener("click", () => {
+    cards.classList.add("list");
+    cards.classList.remove("grid");
+})
