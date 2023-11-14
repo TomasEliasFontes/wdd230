13bade8d3891b7b98218f5c671ced7ff3578db36
tomaseliasfontes <tomaseliasfontes@gmail.com>
2023-11-14 19:04:12 -0300

    weather assigment w10

---
+++ lesson10/scripts/weather.js
@@ -0,0 +1,36 @@
+// select elements of the doc
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=imperial&appid=0aa3f21cc59fecbbbe7cb27b71f722f5';
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconSrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconSrc);
+    weatherIcon.setAttribute('alt',`${desc} icon`);
+    captionDesc.textContent = `${desc}`;
+  }
+
+async function apiFetch(){
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayResults(data);
+        }
+        else {
+            throw Error(await response.text ());
+        }
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetch();
+
+
