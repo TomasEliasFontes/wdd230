92af68351cbef3147758376552c31fd835a46870
tomaseliasfontes <tomaseliasfontes@gmail.com>
2023-11-23 18:58:29 -0300

    add comment to said about the join part

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -7,4 +7,5 @@ document.querySelector("#currentyear").innerHTML = `${year}`;
 let date = document.lastModified;
 document.querySelector("#lastmodified").innerHTML = `Last modified : ${date}`;
 
+// This is just for the join page
 document.querySelector("#timestamp").innerHTML = d;
