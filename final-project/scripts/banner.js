e0669d257c23db33159cc86773b6a3862383276b
tomaseliasfontes <tomaseliasfontes@gmail.com>
2023-12-13 21:49:30 -0300

    add 0 to body and fix banner

--- final-project/scripts/banner.js
+++ final-project/scripts/banner.js
@@ -1,16 +1,7 @@
 const banner = document.getElementById("banner-container");
 const close = document.getElementById("close");
 
-// The day in number
-const today = new Date().getDay();
-
-// If day is in one of the opcion it displey banner
-if (today === 1 || today === 2 || today === 3 ) {
-  banner.style.display = "block";
-}
-else{
-    banner.style.display = "none";
-}
+banner.style.display = "block";
 
 close.addEventListener('click', () => {
   banner.style.display = "none";
